4c82c49268ed1ff1bf485defeecc2e73372dfd9a
petrosvoivontas <petrosvoivontas@gmail.com>
2023-12-09 15:43:26 +0200

    Implement saveTask in TasksUseCaseImpl

--- src/main/java/gr/hua/dit/oop2_ex/usecase/TasksUseCaseImpl.java
+++ src/main/java/gr/hua/dit/oop2_ex/usecase/TasksUseCaseImpl.java
@@ -2,10 +2,15 @@ package gr.hua.dit.oop2_ex.usecase;
 
 import gr.hua.dit.oop2_ex.model.Task;
 import gr.hua.dit.oop2_ex.parser.tasks.TasksParser;
+import net.fortuna.ical4j.data.CalendarOutputter;
 import net.fortuna.ical4j.model.Calendar;
 import net.fortuna.ical4j.model.Component;
 import net.fortuna.ical4j.model.component.VToDo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.function.Predicate;
@@ -40,6 +45,19 @@ public class TasksUseCaseImpl implements TasksUseCase {
 	}
 
 	@Override
-	public void saveTask(Task task) {
+	public void saveTask(Task task, File calendarFile) {
+		VToDo todo = tasksParser.transformToVTodo(task);
+
+		calendar.getComponents().add(todo);
+		CalendarOutputter outputter = new CalendarOutputter();
+		try {
+			FileOutputStream fileOutputStream = new FileOutputStream(calendarFile);
+			outputter.output(calendar, fileOutputStream);
+			fileOutputStream.close();
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
